6b9fb22ed09c1bd6f158b28bf1942dbc6fa9c2bb
Wilfred Hughes <me@wilfred.me.uk>
2015-05-16 12:27:22 +0100

    Check that URLs are accessible.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,40 @@
+extern crate hyper;
+
+use hyper::Client;
+use hyper::status::StatusCode;
+
 use std::env;
+
+#[derive(Debug)]
+enum UrlState {
+    Accessible,
+    BadPath,
+    BadDomain
+}
+
+fn url_accessible(url: &str) -> UrlState {
+    let mut client = Client::new();
+
+    let response = client.get(url).send();
+
+    match response {
+        Ok(r) => {
+            match r.status {
+                StatusCode::Ok => UrlState::Accessible,
+                // TODO: allow redirects unless they're circular
+                _ => UrlState::BadPath
+            }
+        }
+        Err(_) => UrlState::BadDomain
+    }
+}
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     if args.len() > 1 {
         let ref url = args[1];
-        println!("url: {}", url);
+        println!("url status: {:?}", url_accessible(url));
+
     } else {
         // TODO: exit non-zero.
         println!("Please provide an URL.")
