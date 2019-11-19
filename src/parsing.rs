208c0ae1a2bc4c2309da9a8fa952994e2235ec49
Wilfred Hughes <wilfred@fb.com>
2019-11-19 18:10:15 +0000

    Check image URLs too

--- src/parsing.rs
+++ src/parsing.rs
@@ -3,11 +3,20 @@ use scraper::{Html, Selector};
 pub fn get_urls(source_str: &str) -> Vec<String> {
     let document = Html::parse_document(source_str);
 
-    let selector = Selector::parse("a").unwrap();
-
     let mut urls = vec![];
+
+    // Extract URLs from anchor tags.
+    let selector = Selector::parse("a").unwrap();
     for node in document.select(&selector) {
         if let Some(url) = node.value().attr("href") {
+            urls.push(url.to_owned());
+        }
+    }
+
+    // Extract URLs from img tags too.
+    let selector = Selector::parse("img").unwrap();
+    for node in document.select(&selector) {
+        if let Some(url) = node.value().attr("src") {
             urls.push(url.to_owned());
         }
     }
